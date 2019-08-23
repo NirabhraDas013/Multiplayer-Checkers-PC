edc439427d2f1fb4abaf02baa934630d4839ab77
NirabhraDas013 <nirabhra12709@gmail.com>
2019-08-23 14:02:10 +0530

    MultiplayerCheckers Project Files

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,151 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System.Net;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { set; get; }
+
+    public GameObject mainMenu;
+    public GameObject serverMenu;
+    public GameObject connectMenu;
+    public GameObject helpMenu;
+    public GameObject aboutMenu;
+
+    public GameObject serverPrefab;
+    public GameObject clientPrefab;
+    public Text hostIPAddressText;
+
+    public InputField nameInput;
+
+    private void Start()
+    {
+        Instance = this;
+        serverMenu.SetActive(false);
+        connectMenu.SetActive(false);
+        helpMenu.SetActive(false);
+        aboutMenu.SetActive(false);
+        DontDestroyOnLoad(gameObject);
+
+        hostIPAddressText.text = GetIP4Address();
+    }
+
+    public void ConnectButton()
+    {
+        mainMenu.SetActive(false);
+        connectMenu.SetActive(true);
+    }
+    public void HostButton()
+    {
+        try
+        {
+            Server s = Instantiate(serverPrefab).GetComponent<Server>();
+            s.Init();
+
+            Client c = Instantiate(clientPrefab).GetComponent<Client>();
+            c.clientName = nameInput.text;
+            c.isHost = true;
+
+            if (c.clientName == "")
+                c.clientName = "Host";
+            c.ConnectToServer("127.0.0.1", 6321);
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.Message);
+        }
+
+        mainMenu.SetActive(false);
+        serverMenu.SetActive(true);
+    }
+    public void ConnectToServerButton()
+    {
+        string hostAddress = GameObject.Find("HostInput").GetComponent<InputField>().text;
+        if (hostAddress == "")
+            hostAddress = "127.0.0.1";
+
+        try
+        {
+            Client c = Instantiate(clientPrefab).GetComponent<Client>();
+            c.clientName = nameInput.text;
+            if (c.clientName == "")
+                c.clientName = "Client";
+            c.ConnectToServer(hostAddress, 6321);
+            connectMenu.SetActive(false);
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.Message);
+        }
+    }
+    public void BackButton()
+    {
+        mainMenu.SetActive(true);
+        serverMenu.SetActive(false);
+        connectMenu.SetActive(false);
+        helpMenu.SetActive(false);
+        aboutMenu.SetActive(false);
+
+        Server s = FindObjectOfType<Server>();
+        if (s != null)
+            Destroy(s.gameObject);
+
+        Client c = FindObjectOfType<Client>();
+        if (c != null)
+            Destroy(c.gameObject);
+    }
+    public void HotseatButton()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void AboutButton()
+    {
+        mainMenu.SetActive(false);
+        aboutMenu.SetActive(true);
+    }
+
+    public void HelpButton()
+    {
+        mainMenu.SetActive(false);
+        helpMenu.SetActive(true);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public static string GetIP4Address()
+    {
+        string IP4Address = String.Empty;
+
+        foreach (IPAddress IPA in Dns.GetHostAddresses(System.Net.Dns.GetHostName()))
+        {
+            if (IPA.AddressFamily.ToString() == "InterNetwork")
+            {
+                IP4Address = IPA.ToString();
+                break;
+            }
+        }
+
+        if (IP4Address != String.Empty)
+        {
+            return IP4Address;
+        }
+
+        foreach (IPAddress IPA in Dns.GetHostAddresses(Dns.GetHostName()))
+        {
+            if (IPA.AddressFamily.ToString() == "InterNetwork")
+            {
+                IP4Address = IPA.ToString();
+                break;
+            }
+        }
+
+        return IP4Address;
+    }
+}
